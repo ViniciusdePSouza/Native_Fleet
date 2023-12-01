0775a04d5897c4035c10f9e6e425b922f420f9a6
Vinícius Souza <dev.souzavinicius@gamil.com>
2023-12-01 12:02:29 -0300

    styles: signIn page styles completed

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,17 @@
+import { Container, Slogan, Title } from "./styles";
+import backgroundImg from "../../assets/background.png";
+import { Button } from "../../components/Button";
+
+export function SignIn() {
+  return (
+
+    <Container source={backgroundImg}>
+      <Title>Native Fleet</Title>
+
+      <Slogan>Gestão de uso de veículos</Slogan>
+
+      <Button title={"Entrar com Google"} />
+    </Container>
+
+  );
+}
