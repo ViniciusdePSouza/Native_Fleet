ec22f54f43182232f994b81b2d13bb315b04d948
Vinícius Souza <dev.souzavinicius@gamil.com>
2023-12-02 12:15:50 -0300

    google service login in progress

--- App.tsx
+++ App.tsx
@@ -9,9 +9,12 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { Loading } from "./src/components/Loading";
+import { ANDROID_CLIENT_ID } from "@env";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  console.log(ANDROID_CLIENT_ID);
 
   if (!fontsLoaded) {
     return <Loading />;
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,17 +1,60 @@
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+
 import { Container, Slogan, Title } from "./styles";
 import backgroundImg from "../../assets/background.png";
 import { Button } from "../../components/Button";
+import { WEB_CLIENT_ID, ANDROID_CLIENT_ID, IOS_CLIENT_ID } from "@env";
+import { useEffect, useState } from "react";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+
+WebBrowser.maybeCompleteAuthSession();
+
+GoogleSignin.configure({
+  scopes: ["email", "profile"],
+  webClientId: WEB_CLIENT_ID,
+  offlineAccess: true,
+});
 
 export function SignIn() {
-  return (
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [_, response, googleSignIn] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ["profile", "email"],
+  });
 
+  async function handleGoogleSignIn() {
+    setIsAuthenticating(true);
+
+    try {
+      await GoogleSignin.hasPlayServices()
+      const response = await GoogleSignin.signIn()
+
+      if(response.idToken){
+        console.log(`ID Token ===> ${response.idToken}`)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+
+    googleSignIn().then((response) => {
+      if (response.type !== "success") {
+        setIsAuthenticating(false);
+      }
+    });
+  }
+  return (
     <Container source={backgroundImg}>
       <Title>Native Fleet</Title>
 
       <Slogan>Gestão de uso de veículos</Slogan>
 
-      <Button title={"Entrar com Google"} />
+      <Button
+        title={"Entrar com Google"}
+        onPress={handleGoogleSignIn}
+        isLoading={isAuthenticating}
+      />
     </Container>
-
   );
 }
